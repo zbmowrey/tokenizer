919ee88819eae8fb4d33185a451c58a1702fadab
Zach Mowrey <zbmowrey@gmail.com>
2016-10-10 17:31:35 -0400

    e Tokenizer.class.php

---
+++ Tokenizer.class.php
@@ -0,0 +1,52 @@
+namespace zbmowrey;
+
+class Tokenizer
+{
+    public $tokens;
+    public $loaded;
+
+    public function __construct()
+    {
+        $this->tokens = array();
+        $this->loaded = false;
+    }
+
+    public function add(&$variable)
+    {
+        $args = func_get_args();
+        array_shift($args);
+        if(count($args) > 0) {
+            foreach($args as $token) {
+                $this->tokens['#'.$token.'#'] = &$variable;
+                $this->tokens['%'.$token.'%'] = &$variable;
+                $this->tokens['{'.$token.'}'] = &$variable;
+                $this->loaded = true;
+            }
+        }
+    }
+
+    public function replace($string)
+    {
+        if($this->loaded) {
+
+            $find = array();
+            $repl = array();
+
+            foreach ($this->tokens as $k => $v) {
+                if(null !== $v) {
+                    $find[] = $k;
+                    $repl[] = $v;
+                }
+            }
+
+            if (count($find) > 0) {
+                $string = str_replace($find, $repl, $string);
+            }
+            unset($find);
+            unset($repl);
+
+        }
+        return $string;
+    }
+
+}
